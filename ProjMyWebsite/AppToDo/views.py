c321461cf99e56e81dc994b87233aceccff74d3b
Gerardo Sandoval <48520321+1sandovalgerardo@users.noreply.github.com>
2019-06-27 14:34:02 -0500

    pep change

--- ProjMyWebsite/AppToDo/views.py
+++ ProjMyWebsite/AppToDo/views.py
@@ -3,6 +3,7 @@ from .models import List
 from .forms import ListForm
 from django.contrib import messages
 
+
 def ToDoListView(request):
     if request.method == "POST":
         form = ListForm(request.POST)
