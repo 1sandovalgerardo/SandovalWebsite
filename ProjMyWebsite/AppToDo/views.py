3098b7c896bf5b6b02635b06efc3c54d4ec0a767
Gerardo Sandoval <48520321+1sandovalgerardo@users.noreply.github.com>
2019-06-26 22:15:55 -0500

    To Do List is Functional at this point.

--- ProjMyWebsite/AppToDo/views.py
+++ ProjMyWebsite/AppToDo/views.py
@@ -1,28 +1,18 @@
 from django.shortcuts import render, redirect
-from django.views.generic import View
-from django.views.generic import TemplateView
-from django.contrib import messages
 from .models import List
 from .forms import ListForm
-
-# Create your views here.
-
-
-#class ToDoList(TemplateView):
-#    template_name = 'todolist.html'
-
+from django.contrib import messages
 
 def ToDoListView(request):
-    form = ListForm()
-    if request.method == 'POST':
-        print('debug1')
+    if request.method == "POST":
         form = ListForm(request.POST)
+
         if form.is_valid():
-            print('debug2')
             form.save()
             all_items = List.objects.all
-            messages.success(request, ('Item has been added to the list!'))
+            messages.success(request,('Item Added'))
             return render(request, 'todolist.html', {'all_items': all_items})
     else:
         all_items = List.objects.all
         return render(request, 'todolist.html', {'all_items': all_items})
+
