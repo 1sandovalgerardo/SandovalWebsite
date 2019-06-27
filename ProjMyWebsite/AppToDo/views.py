428beecbcf485e82931c8a25f3d2fa5df2004565
Gerardo Sandoval <48520321+1sandovalgerardo@users.noreply.github.com>
2019-06-27 14:24:29 -0500

    final step of getting the todolist working.  it is now working.

--- ProjMyWebsite/AppToDo/views.py
+++ ProjMyWebsite/AppToDo/views.py
@@ -10,16 +10,43 @@ def ToDoListView(request):
         if form.is_valid():
             form.save()
             all_items = List.objects.all
-            messages.success(request,('Item Added'))
+            messages.success(request, ('Item Added'))
             return render(request, 'todolist.html', {'all_items': all_items})
     else:
         all_items = List.objects.all
         return render(request, 'todolist.html', {'all_items': all_items})
+
+
+def Edit(request, pk):
+    if request.method == 'POST':
+        item = List.objects.get(pk=pk)
+        form = ListForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Item has been changed'))
+            return redirect('/todolist')
+    else:
+        item = List.objects.get(pk=pk)
+        return render(request, 'edit.html', {'item': item})
+
 
 def Delete(request, pk):
     item = List.objects.get(pk=pk)
     item.delete()
     messages.success(request, ('Item has been deleted'))
     return redirect('/todolist')
-#    return render(request, 'todolist.html')
+
+
+def cross_off(request, pk):
+    item = List.objects.get(pk=pk)
+    item.completed = True
+    item.save()
+    return redirect('/todolist')
+
+
+def uncross(request, pk):
+    item = List.objects.get(pk=pk)
+    item.completed = False
+    item.save()
+    return redirect('/todolist')
 
