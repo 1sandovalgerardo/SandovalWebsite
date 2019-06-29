72ea1cf3603c3c4ddf32c0590c2635127dcc7f01
Gerardo Sandoval <48520321+1sandovalgerardo@users.noreply.github.com>
2019-06-29 10:23:56 -0500

    started building out forms.py

--- ProjMyWebsite/AppBlog/forms.py
+++ ProjMyWebsite/AppBlog/forms.py
@@ -1,2 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from django import forms
+from AppBlog.models import PostModel,Comment
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta():
+        model = PostModel
+        fields = ('author', 'title', 'text')
+        # widget to grab a particular field
+        widgets = {'title': forms.TextInput(attrs={'class': 'textinput'}),
+                   'text': forms.Textarea(attrs={'class': 'editable\
+                                                 medium-editor-textarea\
+                                                 postcontent'})}
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Comment
+        fields = ('author', 'text')
+
+
+
