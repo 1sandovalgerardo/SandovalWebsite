8d6b8f4e336380985c2cb400bcf636dece802f15
Gerardo Sandoval <48520321+1sandovalgerardo@users.noreply.github.com>
2019-10-16 22:23:34 -0500

    code to make page live
    added the needed code for the links to work and the page to be viewed.

--- ProjMyWebsite/AppCorrStudy/views.py
+++ ProjMyWebsite/AppCorrStudy/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class CorrStudyView(TemplateView):
+    template_name = 'corrstudy.html'
+    
